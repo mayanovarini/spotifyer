800875d7cff0f525be805a817c976b827d7dfc45
thegirlfromipanema <mayanovarini@gmail.com>
2018-03-13 16:57:08 -0700

    fix render on searchresults

--- src/Components/SearchResults/SearchResults.js
+++ src/Components/SearchResults/SearchResults.js
@@ -4,10 +4,12 @@ import TrackList from '../TrackList/TrackList';
 
 class SearchResults extends React.Component {
   render() {
-    <div className="SearchResults">
+    return(
+      <div className="SearchResults">
       <h2>Results</h2>
       <!-- Add a TrackList component -->
     </div>
+    );
   }
 }
 
