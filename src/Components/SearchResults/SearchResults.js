3c82fad3bb9fde3a48a542dc4712cce28e6a257d
thegirlfromipanema <mayanovarini@gmail.com>
2018-03-13 15:38:33 -0700

    add searchresults component

---
+++ src/Components/SearchResults/SearchResults.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react';
+import Playlist from '../Playlist/Playlist';
+
+class SearchResults extends React.Component {
+  render() {
+    <div className="SearchResults">
+      <h2>Results</h2>
+      <!-- Add a TrackList component -->
+    </div>
+  }
+}
+
+export default SearchResults;
