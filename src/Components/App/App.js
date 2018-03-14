24711026110d4f7073a244fdd4e277550811bec5
thegirlfromipanema <mayanovarini@gmail.com>
2018-03-13 23:49:30 -0700

    change colors

--- src/Components/App/App.js
+++ src/Components/App/App.js
@@ -4,16 +4,36 @@ import SearchBar from '../SearchBar/SearchBar';
 import Playlist from '../Playlist/Playlist';
 import SearchResults from '../SearchResults/SearchResults';
 
+const track = {
+  name: "Oakheart",
+  artist: "Poldoore",
+  album: "Dreamworks - EP"
+};
+
+
 class App extends Component {
+
+  constructor(props){
+    super(props);
+    this.state = {
+      searchResults : [
+        track,
+        track,
+        track,
+        track,
+        track
+      ]
+    }
+  }
   render() {
     return (
       <div>
         <h1>Spotifyer</h1>
         <div className="App">
-          // add a SearchBar component
+          <SearchBar />
           <div className="App-playlist">
-           // Add a SearchResults component -->
-           // Add a Playlist component -->
+           <SearchResults searchResults={this.state.searchResults} />
+           <Playlist />
           </div>
         </div>
       </div>
