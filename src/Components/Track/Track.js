81ef8ebdcbe51ed335ffc3aba1d0d679fdddb210
thegirlfromipanema <mayanovarini@gmail.com>
2018-03-13 18:10:56 -0700

    add track component

---
+++ src/Components/Track/Track.js
@@ -0,0 +1,26 @@
+import React, {Component} from 'react';
+import './Track.css';
+
+class Track from React.Component {
+
+  renderAction(){
+    if(isRemoval) {
+      return <a className="Track-action" href="#">-</a>;
+    }
+    return <a className="Track-action" href="#">+</a>;
+  }
+
+  render(){
+    return(
+      <div className="Track">
+        <div className="Track-information">
+          <h3><!-- track name will go here --></h3>
+          <p><!-- track artist will go here--> | <!-- track album will go here --></p>
+        </div>
+        <a className="Track-action"><!-- + or - will go here --></a>
+      </div>
+    );
+  }
+}
+
+export default Track;
