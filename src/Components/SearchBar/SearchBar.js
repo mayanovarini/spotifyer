df7e724725fb1b2d761f2f92aace49a33d02dc27
thegirlfromipanema <mayanovarini@gmail.com>
2018-03-13 17:49:20 -0700

    fix return statement

--- src/Components/SearchBar/SearchBar.js
+++ src/Components/SearchBar/SearchBar.js
@@ -2,10 +2,12 @@ import React, { Component } from 'react';
 
 class SearchBar extends React.Component {
   render(){
-    <div className="SearchBar">
-      <input placeholder="Enter A Song, Album, or Artist" />
-      <a>SEARCH</a>
-    </div>
+    return(
+      <div className="SearchBar">
+        <input placeholder="Enter A Song, Album, or Artist" />
+        <a>SEARCH</a>
+      </div>
+    );
   }
 }
 
